6612aff5ee057d0488a4370235692b041b3000d8
TimoRichert <t.p.richert@gmail.com>
2025-04-06 10:45:07 +0200

    Many updates: More texts completed, "Eigenverbrauch" now included.

--- weg-solar.py
+++ weg-solar.py
@@ -5,20 +5,46 @@ import numpy as np
 import numpy_financial as npf
 from pandas.core.strings.accessor import cat_core
 
-nf = "{:.0f}"
-nfp = "{:.0f}"
+###### PARAMETERS ######
 
-einspeiseverguetung_ve = 11.5 # [ct/kWh]
-einspeiseverguetung_ue = 8.0 # [ct/kWh]
 mieterstromzuschlag = 2.5 # [ct/kWh]
 
+specific_production = 900 # [kWh/kWp]
+
+list_konzepte = ['Ohne Solar', 'Volleinspeisung', 'Gemeinschaftliche Gebäudeversorgung', ' Mieterstrom']
+
+einspeiseverguetung = [0, 11.5 / 100, 8.0 / 100, 8.0 / 100] # [EUR/kWh]
+mieterstromzuschlag = [0, 0, 0, 2.5 / 100] # [EUR/kWh]
+
+operating_cost_fraction = [0, 0.01, 0.01, 0.01]
+
+###### FUNCTIONS ######
+
+def eigenverbrauch(capacity_kWp, power_consumption):
+    # Polynom EV, abgeleitet aus HTW-Rechner:
+    # y = 0.1411x6 - 1.0872x5 + 3.3682x4 - 5.4162x3 + 4.9424x2 - 2.7492x + 1.1003  / R^2 = 1
+    # y = Eigenverbrauchsanteil / x = Größe PV-Anlage relativ zu Anlagengröße [Wp/kWh] / Polynom nur definiert für x = 0 bis 2
+    x = capacity_kWp * 10**3 / power_consumption
+    if (x == 0) or (x > 2):
+        y = 0
+    else:
+        y = 0.1411*x**6 - 1.0872*x**5 + 3.3682*x**4 - 5.4162*x**3 + 4.9424*x**2 - 2.7492*x + 1.1003
+    return y
+
+def warnung(text):
+    st.markdown('_:warning: **HINWEIS:** ' + text + '_')
+
+def zahlenformat(zahl):
+    # return locale.format("%.0f", zahl, grouping=True)
+    return "{:,.0f}".format(zahl)
+
+###### MAIN CONTENT ######
 st.title(':sunny: Solar für _Wohnungs-eigentümergemeinschaften_')
 st.markdown('Ihr seid eine WEG und wollt eine Solaranlage bauen, wisst aber nicht ob das für euch Sinn macht oder wie das geht? Dann seid ihr hier richtig! :smile: Mit diesem einfachen Rechner könnte ihr zügig analysieren ob eine Solaranlage für euch Sinn macht. Los geht''s... :sun_with_face: ')
 
 st.badge("ACHTUNG! Dies ist aktuell nur ein Prototyp mit begrenzter Funktionsfähigkeit.", color="red")
-st.badge("Einiges funktioniert noch nicht (Schätzung Solar-Output, Rechnung Eigenverbrauch und Weiteres)", color="red")
-st.badge("Einige Funktion fehlen und sollen noch kommen (z.B. ""Export als Beschlußvorlage"")", color="red")
-st.badge("Vieles ist noch nicht abschließend sauber geprüft (im Prinzip alles :smile:)", color="red")
+st.badge("Einiges funktioniert noch nicht (Schätzung Solar-Output als Funktion der Modulausrichtung, etc.)", color="red")
+st.badge("Vieles ist noch nicht abschließend sauber geprüft...", color="red")
 st.badge("Schreibt mir gerne Ideen und Feedback: t.p.richert@gmail.com.", color="red")
 
 #st.sidebar.button('Einführung')
@@ -28,6 +54,8 @@ st.badge("Schreibt mir gerne Ideen und Feedback: t.p.richert@gmail.com.", color=
 
 
 st.header('Euer Haus', anchor='haus')
+st.markdown('Macht hier Angaben zum Ist-Zustand bei euch in der WEG')
+
 number_apartments = st.number_input('Anzahl der Wohnungen in eurem Haus:', min_value=0, max_value=50, value=14, key='we')
 power_consumption_per_apartment = st.number_input('Stromverbrauch pro Wohnung im Jahr [kWh] (im Schnitt könnt ihr im Mehrfamilienhaus von ca. 2500 kWh/Jahr pro Haushalt ausgehen. Singles natürlich etwas weniger, Familien etwas mehr.)', min_value=0, max_value=10000, value=2500, key='stromverbrauch')
 number_meters = number_apartments + 1
@@ -38,18 +66,24 @@ power_base_price = st.number_input('Mittlere monatliche Grundgebühr pro Wohnung
 power_consumption = number_apartments * power_consumption_per_apartment * 1.1 # [kWh]
 cost_power_annual = power_consumption * power_price / 100 + 12 * number_meters * power_base_price
 
-st.write('Ihr verbraucht in eurem Haus ca. __' + str(nf.format(power_consumption)) + ' kWh__ Strom pro Jahr (inklusive zusätzlich ca. 10% für den Allgemeinstrom). Dafür bezahlt ihr mit Grundgebühren und Arbeitspreis __ca. ' + str(nf.format(cost_power_annual)) + ' EUR__ im Jahr.')
+st.write('Ihr verbraucht in eurem Haus ca. __' + str(zahlenformat(power_consumption)) + ' kWh__ Strom pro Jahr (inklusive zusätzlich ca. 10% für den Allgemeinstrom). Dafür bezahlt ihr mit Grundgebühren und Arbeitspreis __ca. ' + str(zahlenformat(cost_power_annual)) + ' EUR__ im Jahr.')
 
 st.header('Eure zukünftige Photovoltaik-Anlage', anchor='anlage')
+st.markdown('''
+Gebt hier an, wie eure zukünftige Anlage aussehen soll.
+''')
 
-st.number_input('Postleitzahl (zur Abschätzung der Sonneneinstrahlung)', min_value=0, max_value=99999, key='plz')
-capacity_kWp = st.number_input('Kapazität der Anlage [kWp]', min_value=0, max_value=200, value=21, key='capacity_kWp')
-st.number_input('Anstellwinkel der Anlage [Grad]', min_value=0, max_value=200, value=10, key='angle_deg', help='Bei Flachdächern wird die Anlage typischerweise mit einem Winkel von 10 bis 15 Grad installiert. Bei Spitzdächern entspricht der Winkel dem Dachwinkel.')
+# st.number_input('Postleitzahl (zur Abschätzung der Sonneneinstrahlung)', min_value=0, max_value=99999, key='plz')
+capacity_kWp = st.number_input('Kapazität der Anlage [kWp] - Auf Google Maps könnt ihr eure freie Dachfläche abschätzen. Pro freiem Quadratmeter können ca. 0.2 kWp installiert werden.', min_value=0, max_value=200, value=21, key='capacity_kWp')
+warnung('Wie viel Kapazität ihr am Ende wirklich - mit Berücksichtigung von Verschattungen - sinnvoll auf dem Dach montieren könnt kann euch abschließend nur ein Fachbetrieb sagen. Hier könnt ihr aber mit einer Annahme starten.')
+
+# st.number_input('Anstellwinkel der Anlage [Grad]', min_value=0, max_value=200, value=10, key='angle_deg', help='Bei Flachdächern wird die Anlage typischerweise mit einem Winkel von 10 bis 15 Grad installiert. Bei Spitzdächern entspricht der Winkel dem Dachwinkel.')
 cost_specific = st.number_input('Spezifische Baukosten der Anlage [EUR/kWp] (typisch sind ca. 1200 bis 1400 EUR/kWp für eine Anlage ohne Speicher)', min_value=1000, max_value=1600, value=1400, key='specific_cost')
 cost_total = cost_specific * capacity_kWp
 
-pv_production = capacity_kWp * 900 # [kWh]
-self_consumption_fraction = 0.5
+pv_production = capacity_kWp * specific_production # [kWh]
+
+self_consumption_fraction = eigenverbrauch(capacity_kWp, power_consumption)
 self_consumption_total = pv_production * self_consumption_fraction
 autarkiegrad = self_consumption_total / power_consumption
 einspeisung_eigenverbrauch = pv_production - self_consumption_total
@@ -57,16 +91,15 @@ einspeisung_voll = pv_production
 
 consumption_reststrom = power_consumption - self_consumption_total
 
-st.write('Die Solaranlage kostet euch __' + str(nf.format(cost_total)) + ' EUR__ und  wird __ca. ' + str(nf.format(pv_production)) + ' kWh__ Strom pro Jahr produzieren.')
-st.write('Bei eurem Jahresverbrauch, und wenn wir ein typisches Mehrfamilienhaus annehmen, werdet ihr davon ca. __' + str(nfp.format(self_consumption_fraction*100)) + '% selbst verbrauchen__ ("Eigenverbrauchsquote"), also ca. ' + str(nf.format(self_consumption_total)) + ' kWh pro Jahr. __Damit deckt ihr euren Bedarf zu ca. ' + str(nfp.format(autarkiegrad*100)) + '% ("Autkariegrad")__. Den restlichen Solarstrom, also ' + str(nf.format(einspeisung_eigenverbrauch)) + ' kWh, speist ihr in das Stromnetz ein.')
-# st.write('(Die Errechnung von Eigenverbrauch und Autarkiegrad basieren auf den Methoden des [Solarrechners](https://solar.htw-berlin.de/rechner/) der [Hochschule für Technik und Wirtschaft Berlin](https://www.htw-berlin.de/))')
+st.write('Die Solaranlage kostet euch __' + str(zahlenformat(cost_total)) + ' EUR__ und  wird __ca. ' + str(zahlenformat(pv_production)) + ' kWh__ Strom pro Jahr produzieren.')
+st.write('Bei eurem Jahresverbrauch werdet ihr davon ca. __' + str(zahlenformat(self_consumption_fraction*100)) + '% selbst verbrauchen__ ("Eigenverbrauchsquote"), also ca. ' + str(zahlenformat(self_consumption_total)) + ' kWh pro Jahr. __Damit deckt ihr euren jährlichen Gesamtbedarf zu ca. ' + str(zahlenformat(autarkiegrad*100)) + '%__ ("Autkariegrad"). Den restlichen Solarstrom, also ' + str(zahlenformat(einspeisung_eigenverbrauch)) + ' kWh, speist ihr in das Stromnetz ein.')
+st.write('(Die Errechnung des Eigenverbrauchs basiert auf den Methoden des [Solarrechners](https://solar.htw-berlin.de/rechner/) der [Hochschule für Technik und Wirtschaft Berlin](https://www.htw-berlin.de/))')
 
 st.header('Nutzung des Solarstroms im Haus', anchor='nutzung')
 
 mk = '''
-
 Es gibt verschiedene Arten, den Strom im Haus zu nutzen. Die Nutzungsarten laufen unter dem Fachbegriff "Messkonzepte" und die drei vermutlich interessantesten Ausprägungen sind:
-- **Volleinspeisung** - Es gibt nur einen Zähler an der PV-Anlage und der Strom wird bilanziell komplett ins Stromnetz eingespeist. Dafür bekommt ihr eine Einspeisevergütung. Ihr bezieht weiterhin euren gesamten Strom von euren Stromversorgern.
+- **Volleinspeisung** - Der Strom wird bilanziell komplett ins Stromnetz eingespeist und daher gibt nur einen Einspeisezähler an der PV-Anlage. Für den eingespeisten Strom bekommt ihr eine Einspeisevergütung. Ihr bezieht weiterhin den Strom für die Wohnungen von euren Stromversorgern.
 - **Gemeinschaftliche Gebäudeversorgung** (GGV) - Jede Wohnung wird mit einem  ["intelligenten Messsystem" bzw. iMsys](https://www.bundesnetzagentur.de/DE/Vportal/Energie/Metering/start.html) ausgestattet (landläufig auch Smart Meter genannt). Hiermit kann euer Stromverbrauch in viertelstundengenauer Auflösung gemessen werden. Mit Hilfe eines Abrechnungsdienstleisters, der diese Solarstrommengen den teilnehmenden Haushalten zurechnet, kann der Solarstrom nun den Wohnungen zugeteilt werden. Gesetzlich geregelt wird die erst seit Mitte 2024 rechtskräftige GGV durch [EnWG §42b](https://www.gesetze-im-internet.de/enwg_2005/__42a.html).
 - **Mieterstrom** - Mit diesem schon etwas etablierteren Konzept kann der Strom ebenfalls in den Wohnungen genutzt werden, und trotz des Begriffes "Mieterstrom" kann dieses Konzept auch für WEGs sehr sinnvoll sein. Im Gegensatz zur GGV, bei der jeder Solarstromnutzer seinen bisherigen Stromvertrag behält, schließt die WEG (oder ein von ihr beauftrager Dienstleister, dazu unten mehr) Stromlieferverträge mit den interessierten Hausbewohnern ab. Auch für dieses Konzept braucht es die richtige Messtechnik. Traditionell wurde das Konzept häufig mit einem "physikalischen Summenzähler" gearbeitet, also mit einer Messung des Hausstromes am Hausanschlusskasten. Mittlerweile etabliert sich aber mehr und mehr der ["virtuelle Summenzähler"](https://www.sfv.de/5-fragen-virtueller-summenzaehler), der wie auch die GGV auf iMsys basiert. Mieterstrom ist im [EnWG §42a](https://www.gesetze-im-internet.de/enwg_2005/__42a.html) reguliert.
 
@@ -79,17 +112,18 @@ st.markdown(mk)
 st.header('Wirtschaftlichkeit', anchor='wirtschaftlichkeit')
 
 st.subheader('Investitionskosten')
-md_investment = '''Mit dem Bau einer Solaranlage tätigt ihr ein Investment, das natürlich mit Kosten verbunden ist:
+md_investment = '''Mit dem Bau einer Solaranlage tätigt ihr ein Investment, das je nach Konzept mit folgenden geschätzten Kosten verbunden ist:
 
 - **Bau Solaranlage** - Dies umfasst den Auftrag an den von euch gewählten Fachbetrieb, der die Solaranlage bauen soll. Dies umfasst Material und Installation der Solarmodule inkl. Befestigungsmittel, Wechselrichter, Verkabelung, Anschluss an das Zählerfeld und Inbetriebnahme der Anlage.
-- **Umsetzung Messkonzept** - 
-- **Einrichtungspauschale Abrechnungsdienstleister** -
+- **Umsetzung Messkonzept** - Dies umfasst den Austausch bzw. Einbau der Zähler. Das Messkonzept kann durch einen wettbewerblichen (wMSB) oder aber durch euren grundzuständigen (gMSB) Messstellenbetreiber umgesetzt werden. Der gMSB stellt bis zu 100 EUR pro Zähler in Rechnung, das ist der maximal erlaubte Betrag gem. REFERENZ und hier unsere Annahme. In unserem Beispiel gehen von der Realisierung eines virtuellen Summenzählers über den gMSB aus.
+- **Einrichtungspauschale Abrechnungsdienstleister** - Der Dienstleister, der euch bei der korrekten Bilanzierung und Abrechnung des Solarstroms unterstützt, verlangt eine Einrichtungspauschale und unterstützt dafür auch bei der sauberen Umsetzung des Messkonzeptes. 
 
-:warning: **HINWEIS:** In vielen älteren Häusern sind noch weitere Umbauten notwendig, die ggf. zusätzlich kosten. Dies solltet ihr von eurem Installationsbetrieb prüfen lassen.
 '''
-st.markdown(md_investment)
 
-chart_investment_x = ['(1) Ohne Solar', '(2) Volleinspeisung', 'Gemeinschaftliche Gebäudeversorgung', 'Mieterstrom']
+st.markdown(md_investment)
+warnung('In manchen und insbesondere in älteren Häusern sind noch weitere Umbauten notwendig, die ggf. zusätzliche Kosten verursachen. Dies solltet ihr von einem Elektroinstallationsbetrieb prüfen lassen.')
+
+chart_investment_x = ['Ohne Solar', 'Volleinspeisung', 'Gemeinschaftliche Gebäudeversorgung', 'Mieterstrom']
 chart_investment_y_label = 'Investitionskosten [EUR]'
 
 cat_inv_solaranlage = "Bau Solaranlage"
@@ -105,12 +139,10 @@ cost_inv_total = [0, 0, 0, 0]
 for i in range (0, 4):
     cost_inv_total[i] = cost_inv_solaranlage[i] + cost_inv_abrechnung[i] + cost_inv_messkonzept[i]
 
-
-
 data_cost_investment = pd.DataFrame(
     {
         "index": [1,2,3,4],
-        "Konzepte": ['Ohne Solar', 'Volleinspeisung', 'Gemeinschaftliche Gebäudeversorgung', ' Mieterstrom'],
+        "Konzepte": list_konzepte,
         cat_inv_messkonzept: cost_inv_messkonzept,
         cat_inv_abrechnung: cost_inv_abrechnung,
         cat_inv_solaranlage: cost_inv_solaranlage
@@ -121,12 +153,12 @@ st.bar_chart(data=data_cost_investment, x='Konzepte', y=['Bau Solaranlage', 'Ums
 st.subheader('Betriebskosten und Einnahmen')
 md_op = '''Im Betrieb lassen sich Ausgaben und Einnahmen der Anlage wie folgt beschreiben:
 
-- **Grundgebühren Stromlieferverträge** - 
-- **Einkauf Netzstrom und Reststrom** - 
-- **Betrieb Zähler (Wohnung + Allgemeinstrom)** - 
-- **Betrieb Zähler Solaranlage** - 
-- **Einnahmen aus der Einspeisevergütung** - 
-- **Einnahmen aus Mieterstromzuschlag** - 
+- **Grundgebühren Stromlieferverträge** - Beim Mieterstrom habt ihr als WEG einen gemeinsamen Stromanschluss und rechnet gem. Mieterstromregeln intern ab. In allen anderen Fällen behält jeder Bewohner seinen Stromliefervertrag.
+- **Einkauf Netzstrom und Reststrom** - Je nach Eigenverbrauchsquote müsste ihr bei Mieterstrom oder GGV natürlich weniger Strom einkaufen. Bei Volleinspeisung kauft ihr weiterhin sämtlichen Strom vom Stromversorger.
+- **Betrieb Zähler für Wohnungen und Allgemeinstrom** - Bei Mieterstrom und GGV bezahlt ihr separat für den Betrieb der Zähler, während diese Gebühr bei Volleinspeisung und ohne Solaranlage in euren Stromlieferverträgen enthalten ist.
+- **Betrieb Zähler Solaranlage** - Die Solaranlage hat einen Einspeisezähler, der ebenfalls zu Buche schlägt.
+- **Einnahmen aus der Einspeisevergütung** - Für den eingespeisten Solarstrom bekommt ihr die Einspeisevergütung von ca. 8 ct/kWh.
+- **Einnahmen aus Mieterstromzuschlag** - Beim Mieterstrom erhaltet für jede im Haus verbrauchte Kilowattstunde Solarstrom den Mieterstromzuschlag in Höhe von ca. 2,5 ct/kWh
 
 Im folgenden Diagramm sind eure voraussichtlichen jährlichen Ausgaben und - als negative Werte - Einnahmen dargestellt:
 '''
@@ -148,36 +180,40 @@ cat_reststrom = "Einkauf Netzstrom/Reststrom"
 cost_op_reststrom = [power_consumption * power_price / 100, power_consumption * power_price / 100,
                      consumption_reststrom * power_price / 100, consumption_reststrom * power_price / 100]
 
+cat_op_anlagenbetrieb = "Betrieb Solaranlage"
+cost_op_anlagenbetrieb = np.multiply(cost_inv_total, operating_cost_fraction)
+
 cat_abrechnung = "Abrechnungsdienstleister"
 cost_op_abrechnung = [0, 0, 5 * 12 * number_meters, 5 * 12 * number_meters]
 
 cat_einnahmen_esv = "Einspeisevergütung"
-cost_op_einnahmen_esv = [0, - einspeisung_voll * einspeiseverguetung_ve / 100, - einspeisung_eigenverbrauch * einspeiseverguetung_ue / 100, - einspeisung_eigenverbrauch * einspeiseverguetung_ue / 100]
+einspeisung = [0, -einspeisung_voll, -einspeisung_eigenverbrauch, -einspeisung_eigenverbrauch]
+cost_op_einnahmen_esv = np.multiply(einspeisung, einspeiseverguetung)
 
 cat_einnahmen_msz = "Mieterstromzuschlag"
-cost_op_einnahmen_msz = [0, 0, 0, - self_consumption_total * mieterstromzuschlag / 100]
-
-
+eigenverbrauch = [0, 0, -self_consumption_total, -self_consumption_total]
+cost_op_einnahmen_msz = np.multiply(eigenverbrauch, mieterstromzuschlag)
 
 cost_op_total = [0, 0, 0, 0]
 for i in range (0, 4):
-    cost_op_total[i] = cost_op_meters[i] + cost_op_solarmeter[i] + cost_op_grundgebuehr[i] + cost_op_reststrom[i] + cost_op_abrechnung[i] + cost_op_einnahmen_esv[i] + cost_op_einnahmen_msz[i]
+    cost_op_total[i] = cost_op_meters[i] + cost_op_solarmeter[i] + cost_op_grundgebuehr[i] + cost_op_reststrom[i] + cost_op_anlagenbetrieb[i] + cost_op_abrechnung[i] + cost_op_einnahmen_esv[i] + cost_op_einnahmen_msz[i]
 
 chart_op_y_label = 'Betriebskosten [EUR/a]'
 data_cost_operation = pd.DataFrame(
     {
         "index": [1,2,3,4],
-        "Konzepte": ['Ohne Solar', 'Volleinspeisung', 'Gemeinschaftliche Gebäudeversorgung', ' Mieterstrom'],
+        "Konzepte": list_konzepte,
         cat_op_meters: cost_op_meters,
         cat_op_solarmeter: cost_op_solarmeter,
         cat_op_grundgebuehr: cost_op_grundgebuehr,
         cat_reststrom: cost_op_reststrom,
+        cat_op_anlagenbetrieb: cost_op_anlagenbetrieb,
         cat_abrechnung: cost_op_abrechnung,
         cat_einnahmen_esv: cost_op_einnahmen_esv,
         cat_einnahmen_msz: cost_op_einnahmen_msz
     }
 )
-st.bar_chart(data=data_cost_operation, x='Konzepte', y=[cat_op_grundgebuehr, cat_reststrom, cat_op_meters, cat_op_solarmeter, cat_abrechnung, cat_einnahmen_esv, cat_einnahmen_msz], y_label=chart_op_y_label, height=500)
+st.bar_chart(data=data_cost_operation, x='Konzepte', y=[cat_op_grundgebuehr, cat_reststrom, cat_op_anlagenbetrieb, cat_op_meters, cat_op_solarmeter, cat_abrechnung, cat_einnahmen_esv, cat_einnahmen_msz], y_label=chart_op_y_label, height=500)
 
 st.subheader('Bewertung')
 st.markdown('Aus den Investitionskosten und den durch die Solaranlage reduzierten Betriebskosten sowie den Einnahmen lassen sich nun wirtschaftliche Kenngrößen herleiten. Die Rendite der Investition sieht wie folgt aus:')
@@ -203,7 +239,7 @@ cat_econ_irr = 'Rendite'
 chart_econ_y_label = 'Rendite [%]'
 data_econ_irr = pd.DataFrame(
     {
-        "Konzepte": ['Ohne Solar', 'Volleinspeisung', 'Gemeinschaftliche Gebäudeversorgung', ' Mieterstrom'],
+        "Konzepte": list_konzepte,
         cat_econ_irr: irr_percent
     }
 )
@@ -216,15 +252,15 @@ cat_econ_payback = 'Amortisationsdauer'
 chart_econ_payback_y_label = 'Amortisationsdauer [a]'
 data_econ_pb = pd.DataFrame(
     {
-        "Konzepte": ['Ohne Solar', 'Volleinspeisung', 'Gemeinschaftliche Gebäudeversorgung', ' Mieterstrom'],
+        "Konzepte": list_konzepte,
         cat_econ_payback: payback
     }
 )
 st.bar_chart(data=data_econ_pb, x='Konzepte', y=[cat_econ_payback], y_label=chart_econ_payback_y_label, height=350)
 
-st.header('So geht es weiter...', anchor='weiter')
+st.header('Wie setzt ihr es um?', anchor='umsetzung')
 
-st.markdown('So...')
+st.markdown('**Weitere Infos zur Beschlussfassung in der WEG und zur Umsetzung folgen...** :sunny:')
 
 st.divider()
 
